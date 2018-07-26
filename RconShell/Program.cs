5ca93c978d4c54b461492a59faca8324d2bb8fbf
Alexander Westberg-Bladh <alexander@challengermode.com>
2018-07-26 14:57:54 +0200

    async Main

--- RconShell/Program.cs
+++ RconShell/Program.cs
@@ -16,36 +16,31 @@ namespace RconShell
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            var task = Task.Run(async () =>
+            String ip = "192.168.2.224";
+            int port = 27015;
+            String password = "rcon";
+            var endpoint = new IPEndPoint(
+                IPAddress.Parse(ip),
+                port
+            );
+
+            var rcon = new RCON(endpoint, password, 1000);
+            await rcon.ConnectAsync();
+            bool connected = true;
+            rcon.OnDisconnected += () =>
             {
-                String ip = "192.168.2.224";
-                int port = 27015;
-                String password = "rcon";
-                var endpoint = new IPEndPoint(
-                    IPAddress.Parse(ip),
-                    port
-                );
+                Console.WriteLine("RCON Disconnected");
+                connected = false;
+            };
 
-                var rcon = new RCON(endpoint, password);
-                bool connected = true;
-                rcon.OnDisconnected += () =>
-                {
-                    connected = false;
-                };
-
-                while (connected)
-                {
-                    String command = Console.ReadLine();
-                    String response = await rcon.SendCommandAsync(command);
-                    Console.WriteLine(response);
-                }
-
-            });
-
-            // .Wait() puts exceptions into an AggregateException, while .GetResult() doesn't
-            task.GetAwaiter().GetResult();
+            while (connected)
+            {
+                String command = Console.ReadLine();
+                String response = await rcon.SendCommandAsync(command);
+                Console.WriteLine(response);
+            }
         }
     }
 }
