b51984672e40b324e13679193ad8be629cb0d58b
Alexander Westberg-Bladh <alexander@challengermode.com>
2018-07-30 11:49:09 +0200

    Add test project

---
+++ src/RconTest/UnitTest.cs
@@ -0,0 +1,31 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RCONServerLib;
+using CoreRCON;
+
+
+namespace CoreRCON.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+
+        [TestCleanup]
+        public void testClean()
+        {
+
+        }
+
+        [TestInitialize]
+        public void testInit()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
