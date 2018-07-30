5feb2a911b12dde4849cd531dfa549a8a7dd591f
Alexander Westberg-Bladh <alexander@challengermode.com>
2018-07-30 14:23:32 +0200

    Add simple tests

--- src/RconTest/UnitTest.cs
+++ src/RconTest/UnitTest.cs
@@ -1,7 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 using RCONServerLib;
 using CoreRCON;
-
+using System.Net;
 
 namespace CoreRCON.Tests
 {
@@ -9,23 +10,50 @@ namespace CoreRCON.Tests
     public class UnitTest
     {
 
+        RemoteConServer rconServer;
+        RCON rconClient;
+
         [TestCleanup]
         public void testClean()
         {
-
+            rconClient.Dispose();
+            //rconServer.StopListening();
         }
 
         [TestInitialize]
-        public void testInit()
+        public async Task testInitAsync()
         {
+            rconServer = new RemoteConServer(IPAddress.Loopback, 27015);
+            rconServer.EmptyPayloadKick = false; //Allow multi packet test command through
+            rconServer.InvalidPacketKick = false;
+            rconServer.Password = "rconpasswordtest";
+            rconServer.StartListening();
+            rconClient = new RCON(IPAddress.Loopback, 27015, rconServer.Password);
+            await rconClient.ConnectAsync();
 
         }
 
 
         [TestMethod]
-        public void TestMethod1()
+        public void testBadAuthAsync()
         {
+            rconClient.Dispose();
+            rconClient = new RCON(IPAddress.Loopback, 27015, "wrong PW");
+            Assert.ThrowsException<AuthenticationException>(() =>
+            {
+                rconClient.ConnectAsync().Wait();
+            });
+        }
 
+        [TestMethod]
+        public async Task testEmptyResponse()
+        {
+            rconServer.CommandManager.Add("Empty", "", (command, arguments) =>
+            {
+                return "";
+            });
+            string response = await rconClient.SendCommandAsync("Empty");
+            Assert.AreEqual("", response);
         }
     }
 }
