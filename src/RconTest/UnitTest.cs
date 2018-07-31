c746baf675da335b854227e9d3ccc790b94e040e
Alexander Westberg-Bladh <alexander@challengermode.com>
2018-07-31 14:02:26 +0200

    Improve tests

--- src/RconTest/UnitTest.cs
+++ src/RconTest/UnitTest.cs
@@ -1,8 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
-using RCONServerLib;
 using CoreRCON;
 using System.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+ * Run tests against a running RCON server
+ * Configure settings before running
+ */
 
 namespace CoreRCON.Tests
 {
@@ -10,50 +17,85 @@ namespace CoreRCON.Tests
     public class UnitTest
     {
 
-        RemoteConServer rconServer;
         RCON rconClient;
+        //Connection settings for server
+        private readonly IPAddress _ip = IPAddress.Parse("127.0.0.1");
+        private readonly ushort _port = 27807;
+        private readonly string _password = "rcon";
 
         [TestCleanup]
         public void testClean()
         {
             rconClient.Dispose();
-            //rconServer.StopListening();
         }
 
         [TestInitialize]
         public async Task testInitAsync()
         {
-            rconServer = new RemoteConServer(IPAddress.Loopback, 27015);
-            rconServer.EmptyPayloadKick = false; //Allow multi packet test command through
-            rconServer.InvalidPacketKick = false;
-            rconServer.Password = "rconpasswordtest";
-            rconServer.StartListening();
-            rconClient = new RCON(IPAddress.Loopback, 27015, rconServer.Password);
+            rconClient = new RCON(_ip, _port, _password);
             await rconClient.ConnectAsync();
 
         }
 
 
         [TestMethod]
-        public void testBadAuthAsync()
+        [ExpectedException(typeof(AuthenticationException))]
+        public async Task testBadAuthAsync()
         {
             rconClient.Dispose();
-            rconClient = new RCON(IPAddress.Loopback, 27015, "wrong PW");
-            Assert.ThrowsException<AuthenticationException>(() =>
-            {
-                rconClient.ConnectAsync().Wait();
-            });
+            rconClient = new RCON(_ip, _port, "wrong PW");
+            await rconClient.ConnectAsync();
         }
 
         [TestMethod]
-        public async Task testEmptyResponse()
+        public async Task testEmptyResponseAsync()
         {
-            rconServer.CommandManager.Add("Empty", "", (command, arguments) =>
-            {
-                return "";
-            });
-            string response = await rconClient.SendCommandAsync("Empty");
+            string response = await rconClient.SendCommandAsync("//comment");
             Assert.AreEqual("", response);
         }
+
+        [TestMethod]
+        public async Task testEchoAsync()
+        {
+            string response = await rconClient.SendCommandAsync("say hi");
+            Assert.AreEqual("Console: hi", response);
+        }
+
+
+        [TestMethod]
+        public async Task testLongResponseAsync()
+        {
+            string response = await rconClient.SendCommandAsync("cvarList");
+            Assert.IsTrue(response.EndsWith("total convars/concommands"));
+        }
+
+
+        [TestMethod]
+        public async Task testMultipleCommands()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string response = await rconClient.SendCommandAsync($"say {i}");
+                Assert.AreEqual($"Console: {i}", response);
+            }
+        }
+
+        [TestMethod]
+        public async Task testCommandsConcurent()
+        {
+            List<Task> tasks = new List<Task>();
+
+            tasks = Enumerable.Range(1, 10)
+                .Select(async (i) =>
+                {
+                    string response = await rconClient.SendCommandAsync($"say {i}");
+                    Console.WriteLine($"recived response {i} : {response}");
+                    Assert.AreEqual($"Console: {i}", response);
+                }).ToList();
+            //Parallel.ForEach(tasks, task => task.Start());
+            await Task.WhenAll(tasks);
+            Console.Out.Flush();
+        }
+
     }
 }
