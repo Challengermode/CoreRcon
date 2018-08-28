bee58694d1a56011336403b01c58e0da35611046
Alexander Westberg-Bladh <alexander@challengermode.com>
2018-08-28 13:34:14 +0200

    Add RoundEndScore Parser

---
+++ src/CoreRCON/Parsers/Csgo/RoundEndScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CoreRCON.Parsers.Csgo
+{
+    public class RoundEndScore : IParseable
+    {
+        public string WinningTeam { get; set; }
+        public int TScore { get; set; }
+        public int CTScore { get; set; }
+    }
+
+    public class RoundEndScoreParser : DefaultParser<RoundEndScore>
+    {
+        public override string Pattern { get; } = @"Team ""(?<winning_team>.+?)"" triggered ""SFUI_Notice_.+?_Win"" \(CT ""(?<ct_score>\d+)""\) \(T ""(?<t_score>\d+)""\)";
+        private static PlayerParser playerParser { get; } = new PlayerParser();
+
+        public override RoundEndScore Load(GroupCollection groups)
+        {
+            return new RoundEndScore
+            {
+                WinningTeam = groups["winning_team"].Value,
+                TScore = int.Parse(groups["t_score"].Value),
+                CTScore = int.Parse(groups["ct_score"].Value),
+            };
+        }
+    }
+}
