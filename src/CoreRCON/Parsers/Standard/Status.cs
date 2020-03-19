e364029e48e64f08bda21c1fe225fae7341cd7fc
Alexander Bladh <albladh@kth.se>
2020-03-19 14:20:52 +0100

    Improve status parser and add test
    Fix: #5,#8

--- src/CoreRCON/Parsers/Standard/Status.cs
+++ src/CoreRCON/Parsers/Standard/Status.cs
@@ -1,51 +1,138 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace CoreRCON.Parsers.Standard
 {
-	public class Status : IParseable
-	{
-		public string Account { get; set; }
-		public byte Bots { get; set; }
-		public ulong CommunityID { get; set; }
-		public string Hostname { get; set; }
-		public byte Humans { get; set; }
-		public string LocalHost { get; set; }
-		public string Map { get; set; }
-		public byte MaxPlayers { get; set; }
-		public string PublicHost { get; set; }
-		public string SteamID { get; set; }
-		public string[] Tags { get; set; }
-		public string Version { get; set; }
-	}
+    public class Status : IParseable
+    {
+        [Obsolete("No longer part of status message")]
+        public string Account { get; set; }
+        public byte Bots { get; set; }
+        public ulong CommunityID { get; set; }
+        public string Hostname { get; set; }
+        public byte Humans { get; set; }
+        public string LocalHost { get; set; }
+        public string Map { get; set; }
+        public byte MaxPlayers { get; set; }
+        public string PublicHost { get; set; }
+        public string SteamID { get; set; }
+        [Obsolete("No longer part of status message")]
+        public string[] Tags { get; set; }
+        public string Version { get; set; }
+        public bool Hibernating { get; set; }
+        public string Type { get; set; }
+    }
 
-	internal class StatusParser : DefaultParser<Status>
-	{
-		public override string Pattern { get; } = @"hostname\s*: (?<Hostname>.+?)
-version\s*: (?<Version>.+?)
-udp\/ip\s*: (?<LocalHost>.+?)  \(public ip: (?<PublicHost>.+?)\)
-steamid\s*: \[(?<SteamID>.+?)\] \((?<CommunityID>\d+)\)
-account\s*: (?<Account>.+?)
-map\s*: (?<Map>.+?) at: .*
-tags\s*: (?<Tags>.+?)
-players\s*: (?<Humans>\d+) humans, (?<Bots>\d+) bots \((?<MaxPlayers>\d+) max\)";
+    public class StatusParser : IParser<Status>
+    {
+        public string Pattern => throw new System.NotImplementedException();
 
-		public override Status Load(GroupCollection groups)
-		{
-			return new Status
-			{
-				Hostname = groups["Hostname"].Value,
-				Version = groups["Version"].Value,
-				LocalHost = groups["LocalHost"].Value,
-				PublicHost = groups["PublicHost"].Value,
-				SteamID = groups["SteamID"].Value,
-				CommunityID = ulong.Parse(groups["CommunityID"].Value),
-				Account = groups["Account"].Value,
-				Map = groups["Map"].Value,
-				Tags = groups["Tags"].Value.Split(','),
-				Humans = byte.Parse(groups["Humans"].Value),
-				Bots = byte.Parse(groups["Bots"].Value),
-				MaxPlayers = byte.Parse(groups["MaxPlayers"].Value)
-			};
-		}
-	}
+        public bool IsMatch(string input)
+        {
+            return input.Contains("hostname: ") | input.Contains("hibernating");
+
+        }
+
+        public Status Load(GroupCollection groups)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Status Parse(string input)
+        {
+            Dictionary<string, string> groups = input.Split('\n')
+                .Select(x => x.Split(':'))
+                .Where(x => x.Length > 1 && !string.IsNullOrEmpty(x[0].Trim())
+                    && !string.IsNullOrEmpty(x[1].Trim()))
+                .ToDictionary(x => x[0].Trim(), x => string.Join(":", x.ToList().Skip(1)).Trim());
+
+            string hostname = null;
+            groups.TryGetValue("hostname", out hostname);
+            string version = null;
+            groups.TryGetValue("version", out version);
+            string steamId = null;
+            if (version != null)
+            {
+                Match match = Regex.Match(version, ".*(\\[.*\\]).*");
+                if (match.Success)
+                {
+                    steamId = match.Groups[1].Value;
+                }
+            }
+            string map = null;
+            groups.TryGetValue("map", out map);
+            string type = null;
+            groups.TryGetValue("type", out type);
+
+            byte players = 0, bots = 0, maxPlayers = 0;
+            string playerString = null;
+            bool hibernating = false;
+            groups.TryGetValue("players", out playerString);
+            if (playerString != null)
+            {
+                Match oldMatch = Regex.Match(playerString, "(\\d+) \\((\\d+) max\\).*"); //Old pattern
+                Match newMatch = Regex.Match(playerString, "(\\d+) humans, (\\d+) bots\\((\\d+)/\\d+ max\\) (\\(not hibernating\\))?.*");
+                if (oldMatch.Success)
+                {
+                    players = byte.Parse(oldMatch.Groups[1].Value);
+                    maxPlayers = byte.Parse(oldMatch.Groups[2].Value);
+                    bots = 0;
+                }
+                else if (newMatch.Success)
+                {
+                    players = byte.Parse(newMatch.Groups[1].Value);
+                    maxPlayers = byte.Parse(newMatch.Groups[3].Value);
+                    bots = byte.Parse(newMatch.Groups[2].Value);
+                    if (newMatch.Groups[4].Success)
+                    {
+                        hibernating = !newMatch.Groups[4].Value.Contains("not hibernating");
+                    }
+                }
+            }
+            else
+            {
+                hibernating = input.Contains("hibernating") && !input.Contains("not hibernating");
+            }
+
+            string localIp = null, publicIp = null, ipString = null;
+            groups.TryGetValue("udp / ip", out ipString);
+            if (ipString != null)
+            {
+                Match oldMatch = Regex.Match(ipString, "((\\d|\\.)+:(\\d|\\.)+)\\(public ip: (.*)\\).*"); //Old pattern
+                Match newMatch = Regex.Match(ipString, "\\((.*:.*)\\)\\s+\\(public ip: (.*)\\).*");
+                if (oldMatch.Success)
+                {
+                    localIp = oldMatch.Groups[1].Value;
+                    publicIp = oldMatch.Groups[4].Value;
+                }
+                else if (newMatch.Success)
+                {
+                    localIp = newMatch.Groups[1].Value;
+                    publicIp = newMatch.Groups[2].Value;
+                }
+            }
+
+            return new Status()
+            {
+                Hostname = hostname,
+                Version = version,
+                SteamID = steamId,
+                Map = map,
+                Type = type,
+                Humans = players,
+                MaxPlayers = maxPlayers,
+                Bots = bots,
+                Hibernating = hibernating,
+                LocalHost = localIp,
+                PublicHost = publicIp
+            };
+        }
+
+        public Status Parse(Group group)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
--- src/RconTest/ParserTest.cs
+++ src/RconTest/ParserTest.cs
@@ -114,6 +114,65 @@ namespace CoreRCON.Tests
             Assert.AreEqual(armor, e.PostArmor);
             Assert.AreEqual(hitgroup, e.HitLocation);
         }
+
+        [TestMethod]
+        public void testL4DStatus()
+        {
+            string test = "hostname: l4d hostname\n" +
+                "version: 2019.11.12 / 24 7671 secure\n" +
+                "udp / ip  : (127.0.0.1:1234)  (public ip: 1.1.1.1)\n" +
+                "map     : deathrun_portal at: 0 x, 0 y, 0 z\n" +
+                "players : 5 (24 max)\n" +
+                "# userid name                uniqueid            connected ping loss state  adr\n" +
+                "id id id";
+            StatusParser parser = new StatusParser();
+            Assert.IsTrue(parser.IsMatch(test));
+            Status status = parser.Parse(test);
+            Assert.IsFalse(status.Hibernating);
+            Assert.AreEqual("1.1.1.1", status.PublicHost);
+            Assert.AreEqual("127.0.0.1:1234", status.LocalHost);
+            Assert.AreEqual("l4d hostname", status.Hostname);
+            Assert.AreEqual("deathrun_portal at: 0 x, 0 y, 0 z", status.Map);
+            Assert.AreEqual(5, status.Humans);
+            Assert.AreEqual(24, status.MaxPlayers);
+        }
+
+        [TestMethod]
+        public void testCsgoStatus()
+        {
+            string test = "hostname: long hostname\n" +
+                "version: 1.37.4.4 / 13744 1081 / 7776 secure[G: 1:3091169]\n" +
+                "udp / ip  : 127.0.0.1:27711(public ip: 1.1.1.1)\n" +
+                "os      :  Linux\n" +
+                "type    :  community dedicated\n" +
+                "map     : de_dust2\n" +
+                "gotv[0]:  port 27712, delay 90.0s, rate 32.0\n" +
+                "players : 2 humans, 1 bots(6/0 max) (not hibernating)\n\n" +
+                "# userid name uniqueid connected ping loss state rate adr\n" +
+                "# 2 \"GOTV\" BOT active 32\n";
+            StatusParser parser = new StatusParser();
+            Assert.IsTrue(parser.IsMatch(test));
+            Status status = parser.Parse(test);
+            Assert.IsFalse(status.Hibernating);
+            Assert.AreEqual("1.1.1.1", status.PublicHost);
+            Assert.AreEqual("127.0.0.1:27711", status.LocalHost);
+            Assert.AreEqual("long hostname", status.Hostname);
+            Assert.AreEqual("de_dust2", status.Map);
+            Assert.AreEqual(2, status.Humans);
+            Assert.AreEqual(6, status.MaxPlayers);
+            Assert.AreEqual(1, status.Bots);
+
+        }
+
+        [TestMethod]
+        public void testHibernatingStatus()
+        {
+            string test = "Server hibernating";
+            StatusParser parser = new StatusParser();
+            Assert.IsTrue(parser.IsMatch(test));
+            Status status = parser.Parse(test);
+            Assert.IsTrue(status.Hibernating);
+        }
     }
 }
 
