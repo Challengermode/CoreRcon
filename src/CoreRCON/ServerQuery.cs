d5f0e3490fa92ddfb1d1b476905323cfd4486988
Alexander Westberg-Bladh <alexander@challengermode.com>
2022-04-15 20:52:51 +0200

    Improve Info Query code
    Add Challenge code to A2S_Info Query, "some" servers will now require a
    challenge to respond to A2S_Info queries see:
    https://developer.valvesoftware.com/wiki/Server_queries#A2S_INFO
    
    Add timeout argument, the method will now timeout server does not
    respond.

--- src/CoreRCON/ServerQuery.cs
+++ src/CoreRCON/ServerQuery.cs
@@ -33,73 +33,84 @@ namespace CoreRCON
             Stat = 0x00
         }
 
-		private static UdpClient _client;
+        private static UdpClient _client;
         private static readonly byte[] _magic = new byte[] { 0xFE, 0xFD }; // Minecraft 'magic' bytes.
         private static readonly byte[] _sessionid = new byte[] { 0x01, 0x02, 0x03, 0x04 };
+        private static readonly byte[] _asInfoPayload = new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E, 0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00 };
+        private static readonly byte[] _asInfochallengeResponse = new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x41 };
 
         static ServerQuery()
-		{
-			_client = new UdpClient();
-		}
+        {
+            _client = new UdpClient();
+        }
 
-		/// <summary>
-		/// Get information about the server.
-		/// </summary>
-		/// <param name="address">IP of the server.</param>
-		/// <param name="port">Port to query gameserver.</param>
+        /// <inheritdoc cref="Info(IPEndPoint, ServerType, TimeSpan?)"/>
+        /// <param name="address">IP of the server.</param>
+        public static async Task<IQueryInfo> Info(IPAddress address, ushort port, ServerType type, TimeSpan? timeout = null)
+            => await Info(new IPEndPoint(address, port), type, timeout);
+
+        /// <summary>
+        /// Get information about the server.
+        /// </summary>
+        /// <param name="host">Endpoint of the server.</param>
         /// <param name="type">Server type.</param>
-		public static async Task<IQueryInfo> Info(IPAddress address, ushort port, ServerType type) => await Info(new IPEndPoint(address, port), type);
-
-		/// <summary>
-		/// Get information about the server.
-		/// </summary>
-		/// <param name="host">Endpoint of the server.</param>
-		public static async Task<IQueryInfo> Info(IPEndPoint host, ServerType type)
-		{
-            switch (type)
+        /// <param name="timeout">Server type.</param>
+        /// <exception cref="TimeoutException"></exception>
+        public static Task<IQueryInfo> Info(IPEndPoint host, ServerType type, TimeSpan? timeout = null)
+        {
+            return Task.Run<IQueryInfo>(async () =>
             {
-                case ServerType.Source:
-                    await _client.SendAsync(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x54, 0x53, 0x6F, 0x75, 0x72, 0x63, 0x65, 0x20, 0x45, 0x6E, 0x67, 0x69, 0x6E, 0x65, 0x20, 0x51, 0x75, 0x65, 0x72, 0x79, 0x00 }, 25, host);
-                    var sourceResponse = await _client.ReceiveAsync();
-                    return SourceQueryInfo.FromBytes(sourceResponse.Buffer);
-                case ServerType.Minecraft:
-                    var padding = new byte[] { 0x00, 0x00, 0x00, 0x00 };
-                    var datagram = _magic.Concat(new[] { (byte)PacketType.Stat }).Concat(_sessionid).Concat(await Challenge(host, ServerType.Minecraft)).Concat(padding).ToArray();
-                    await _client.SendAsync(datagram, datagram.Length, host);
-                    var mcResponce = await _client.ReceiveAsync();
-                    return MinecraftQueryInfo.FromBytes(mcResponce.Buffer);
-                default:
-                    throw new ArgumentException("type argument was invalid");
-            }
-			
-		}
+                switch (type)
+                {
+                    case ServerType.Source:
+                        await _client.SendAsync(_asInfoPayload, _asInfoPayload.Length, host);
+                        UdpReceiveResult sourceResponse = await _client.ReceiveAsync();
+                        // If Server responds with a Challenge number we need to resend the request with that number
+                        if (sourceResponse.Buffer.ToArray().Take(_asInfochallengeResponse.Length).SequenceEqual(_asInfochallengeResponse))
+                        {
+                            byte[] challenge = _asInfoPayload.Concat(sourceResponse.Buffer.Skip(5).Take(4)).ToArray();
+                            await _client.SendAsync(challenge, challenge.Length, host);
+                            sourceResponse = await _client.ReceiveAsync();
+                        }
+                        return SourceQueryInfo.FromBytes(sourceResponse.Buffer);
+                    case ServerType.Minecraft:
+                        var padding = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+                        var datagram = _magic.Concat(new[] { (byte)PacketType.Stat }).Concat(_sessionid).Concat(await Challenge(host, ServerType.Minecraft)).Concat(padding).ToArray();
+                        await _client.SendAsync(datagram, datagram.Length, host);
+                        var mcResponce = await _client.ReceiveAsync();
+                        return MinecraftQueryInfo.FromBytes(mcResponce.Buffer);
+                    default:
+                        throw new ArgumentException("type argument was invalid");
+                }
+            }).TimeoutAfter(timeout);
+        }
 
-		/// <summary>
-		/// Get information about each player currently on the server.
-		/// </summary>
-		/// <param name="address">IP of the server.</param>
-		/// <param name="port">Port to query gameserver.</param>
-		public static async Task<ServerQueryPlayer[]> Players(IPAddress address, ushort port) => await Players(new IPEndPoint(address, port));
+        /// <summary>
+        /// Get information about each player currently on the server.
+        /// </summary>
+        /// <param name="address">IP of the server.</param>
+        /// <param name="port">Port to query gameserver.</param>
+        public static async Task<ServerQueryPlayer[]> Players(IPAddress address, ushort port) => await Players(new IPEndPoint(address, port));
 
-		/// <summary>
-		/// Get information about each player currently on the server.
-		/// </summary>
-		/// <param name="host">Endpoint of the server.</param>
-		public static async Task<ServerQueryPlayer[]> Players(IPEndPoint host)
-		{
-			var challenge = new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x55 }.Concat(await Challenge(host, ServerType.Source)).ToArray();
-			await _client.SendAsync(challenge, 9, host);
-			var response = await _client.ReceiveAsync();
-			return ServerQueryPlayer.FromBytes(response.Buffer);
-		}
+        /// <summary>
+        /// Get information about each player currently on the server.
+        /// </summary>
+        /// <param name="host">Endpoint of the server.</param>
+        public static async Task<ServerQueryPlayer[]> Players(IPEndPoint host)
+        {
+            var challenge = new byte[] { 0xFF, 0xFF, 0xFF, 0xFF, 0x55 }.Concat(await Challenge(host, ServerType.Source)).ToArray();
+            await _client.SendAsync(challenge, 9, host);
+            var response = await _client.ReceiveAsync();
+            return ServerQueryPlayer.FromBytes(response.Buffer);
+        }
 
-		/// <summary>
-		/// Send a challenge request to the server and receive a code.
-		/// </summary>
-		/// <param name="host">Endpoint of the server.</param>
-		/// <returns>Challenge code to use with challenged requests.</returns>
-		private static async Task<byte[]> Challenge(IPEndPoint host, ServerType type)
-		{
+        /// <summary>
+        /// Send a challenge request to the server and receive a code.
+        /// </summary>
+        /// <param name="host">Endpoint of the server.</param>
+        /// <returns>Challenge code to use with challenged requests.</returns>
+        private static async Task<byte[]> Challenge(IPEndPoint host, ServerType type)
+        {
             switch (type)
             {
                 case ServerType.Source:
@@ -119,7 +130,7 @@ namespace CoreRCON
                 default:
                     throw new ArgumentException("type argument was invalid");
             }
-            
-		}
-	}
+
+        }
+    }
 }
