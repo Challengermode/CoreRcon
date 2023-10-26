775ed63d56f5f0ef40502b38525cb3a618e16400
Alexander Westberg-Bladh <alexander@challengermode.com>
2023-10-26 10:48:03 +0200

    Observer Reader & Writer exceptions correctly
    The Task.WhenAny was hideing the exceptions of the inner tasks.
    By awaiting the returned task from WhenAny we will now observer
    the inner tasks exception correctly.
    Thanks @arildboifot for reporting.
    Fix: #45

--- src/CoreRCON/RCON.cs
+++ src/CoreRCON/RCON.cs
@@ -37,10 +37,11 @@ namespace CoreRCON
         private Dictionary<int, string> _incomingBuffer { get; } = new Dictionary<int, string>();
 
         private Socket _tcp { get; set; }
-        private Task _networkConsumerTask;
 
         private readonly ILogger _logger;
         SemaphoreSlim _semaphoreSlim;
+        private Task _socketWriter;
+        private Task _socketReader;
 
         /// <summary>
         /// Fired if connection is lost
@@ -102,25 +103,17 @@ namespace CoreRCON
                 .ConfigureAwait(false);
             _connected = true;
             Pipe pipe = new Pipe();
-            Task writing = FillPipeAsync(pipe.Writer);
-            Task reading = ReadPipeAsync(pipe.Reader);
+
+            _socketWriter = FillPipeAsync(pipe.Writer)
+                .ContinueWith(LogDisconnect);
+            _socketReader = ReadPipeAsync(pipe.Reader)
+                .ContinueWith(LogDisconnect);
 
             // Wait for successful authentication
             _authenticationTask = new TaskCompletionSource<bool>();
             await SendPacketAsync(new RCONPacket(0, PacketType.Auth, _password))
                 .ConfigureAwait(false);
-            _networkConsumerTask = Task.WhenAll(writing, reading)
-                .ContinueWith(t =>
-                {
-                    if (_connected)
-                    {
-                        var aggException = t.Exception.Flatten();
-                        _logger?.LogError("RCON connection closed");
-                        foreach (var exception in aggException.InnerExceptions)
-                            _logger?.LogError($"Exception {exception.Message}");
-                    }
-                },
-                TaskContinuationOptions.OnlyOnFaulted);
+
             try
             {
                 await _authenticationTask.Task
@@ -186,70 +179,75 @@ namespace CoreRCON
         /// <returns>Consumer Task</returns>
         async Task ReadPipeAsync(PipeReader reader)
         {
-            while (true)
+            try
             {
-                ReadResult result = await reader.ReadAsync()
-                    .ConfigureAwait(false);
-                ReadOnlySequence<byte> buffer = result.Buffer;
-                SequencePosition packetStart = buffer.Start;
-
-                if (buffer.Length < 4)
+                while (true)
                 {
-                    if (result.IsCompleted)
-                    {
-                        break;
-                    }
-                    reader.AdvanceTo(packetStart, buffer.End);
-                    continue;
-                    // Complete header not yet received
-                }
-                int size = BitConverter.ToInt32(buffer.Slice(packetStart, 4).ToArray(), 0);
-                if (buffer.Length >= size + 4)
-                {
-                    // Get packet end positions 
-                    SequencePosition packetEnd = buffer.GetPosition(size + 4, packetStart);
-                    byte[] byteArr = buffer.Slice(packetStart, packetEnd).ToArray();
-                    RCONPacket packet = RCONPacket.FromBytes(byteArr);
+                    ReadResult result = await reader.ReadAsync()
+                        .ConfigureAwait(false);
+                    ReadOnlySequence<byte> buffer = result.Buffer;
+                    SequencePosition packetStart = buffer.Start;
 
-                    if (packet.Type == PacketType.AuthResponse)
+                    if (buffer.Length < 4)
                     {
-                        // Failed auth responses return with an ID of -1
-                        if (packet.Id == -1)
+                        if (result.IsCompleted)
                         {
-                            _authenticationTask.SetException(
-                                new AuthenticationException($"Authentication failed for {_tcp.RemoteEndPoint}.")
-                                );
+                            break;
                         }
-                        // Tell Connect that authentication succeeded
-                        _authenticationTask.SetResult(true);
+                        reader.AdvanceTo(packetStart, buffer.End);
+                        continue;
+                        // Complete header not yet received
+                    }
+                    int size = BitConverter.ToInt32(buffer.Slice(packetStart, 4).ToArray(), 0);
+                    if (buffer.Length >= size + 4)
+                    {
+                        // Get packet end positions 
+                        SequencePosition packetEnd = buffer.GetPosition(size + 4, packetStart);
+                        byte[] byteArr = buffer.Slice(packetStart, packetEnd).ToArray();
+                        RCONPacket packet = RCONPacket.FromBytes(byteArr);
+
+                        if (packet.Type == PacketType.AuthResponse)
+                        {
+                            // Failed auth responses return with an ID of -1
+                            if (packet.Id == -1)
+                            {
+                                _authenticationTask.SetException(
+                                    new AuthenticationException($"Authentication failed for {_tcp.RemoteEndPoint}.")
+                                    );
+                            }
+                            // Tell Connect that authentication succeeded
+                            _authenticationTask.SetResult(true);
+                        }
+
+                        // Forward rcon packet to handler
+                        RCONPacketReceived(packet);
+
+                        reader.AdvanceTo(packetEnd);
+                    }
+                    else
+                    {
+                        reader.AdvanceTo(packetStart, buffer.End);
                     }
 
-                    // Forward rcon packet to handler
-                    RCONPacketReceived(packet);
+                    // Tell the PipeReader how much of the buffer we have consumed
 
-                    reader.AdvanceTo(packetEnd);
-                }
-                else
-                {
-                    reader.AdvanceTo(packetStart, buffer.End);
-                }
-
-                // Tell the PipeReader how much of the buffer we have consumed
-
-                // Stop reading if there's no more data coming
-                if (buffer.IsEmpty && result.IsCompleted)
-                {
-                    break; // exit loop
+                    // Stop reading if there's no more data coming
+                    if (buffer.IsEmpty && result.IsCompleted)
+                    {
+                        break; // exit loop
+                    }
                 }
             }
+            finally
+            {
+                // If authentication did not complete
+                _authenticationTask.TrySetException(
+                                    new AuthenticationException($"Server did not respond to auth {_tcp.RemoteEndPoint}.")
+                                    );
 
-            // If authentication did not complete
-            _authenticationTask.TrySetException(
-                                new AuthenticationException($"Server did not respond to auth {_tcp.RemoteEndPoint}.")
-                                );
-
-            // Mark the PipeReader as complete
-            await reader.CompleteAsync().ConfigureAwait(false);
+                // Mark the PipeReader as complete
+                await reader.CompleteAsync().ConfigureAwait(false);
+            }
         }
 
         public void Dispose()
@@ -299,9 +297,9 @@ namespace CoreRCON
             // However we this library is designed to be able to run without its own thread
             // Read more about this option here:
             // https://github.com/davidfowl/AspNetCoreDiagnosticScenarios/blob/master/AsyncGuidance.md#always-create-taskcompletionsourcet-with-taskcreationoptionsruncontinuationsasynchronously
-            var source = new TaskCompletionSource<string>();
+            var completionSource = new TaskCompletionSource<string>();
             int packetId = Interlocked.Increment(ref _packetId);
-            if (!_pendingCommands.TryAdd(packetId, source))
+            if (!_pendingCommands.TryAdd(packetId, completionSource))
             {
                 throw new SocketException();
             }
@@ -310,10 +308,11 @@ namespace CoreRCON
 
 
             await _semaphoreSlim.WaitAsync();
+            Task completedTask;
             try
             {
                 await SendPacketAsync(packet).ConfigureAwait(false);
-                await Task.WhenAny(source.Task, _networkConsumerTask)
+                completedTask = await Task.WhenAny(completionSource.Task, _socketWriter, _socketReader)
                     .TimeoutAfter(TimeSpan.FromMilliseconds(_timeout))
                     .ConfigureAwait(false);
             }
@@ -328,19 +327,14 @@ namespace CoreRCON
                 _incomingBuffer.Remove(packet.Id);
             }
 
-            if (source.Task.IsCompleted)
+            if (completedTask == completionSource.Task)
             {
-                return source.Task.Result;
+                return await completionSource.Task;
             }
 
-            if (_networkConsumerTask.IsFaulted)
-            {
-                throw _networkConsumerTask.Exception.InnerException;
-            }
-            else
-            {
-                throw new SocketException();
-            }
+            // Observe exception
+            await completedTask;
+            throw new SocketException();
         }
 
         /// <summary>
@@ -402,6 +396,18 @@ namespace CoreRCON
                     .ConfigureAwait(false);
             }
 
+        }
+
+        private void LogDisconnect(Task task)
+        {
+            if (_connected)
+            {
+                _logger?.LogError("RCON connection closed");
+                if (task.IsFaulted)
+                {
+                    _logger?.LogError($"Exception {task.Exception.Message}");
+                }
+            }
         }
     }
 }
